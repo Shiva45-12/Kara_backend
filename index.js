cbaaf6069249cb90e5bb17e47ab9d242071daadf
SHIVA VISHWAKARMA <raw66010@gmail.com>
2025-12-25 11:14:17 +0530

    cors done

--- index.js
+++ index.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://kara-frontend.onrender.com", "http://localhost:5000"],
+    credentials: true,
+  }
+));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
